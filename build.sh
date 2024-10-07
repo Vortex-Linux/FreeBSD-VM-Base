34d086616059d46d77827fecf324aff3709d452e
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-07 23:35:12 +0530

    Made some basic changes(nothing is tested as of now)

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+XML_FILE="/tmp/freebsd-vm-base.xml"
+
+LATEST_IMAGE=$(lynx -dump -listonly -nonumbers https://bsd-cloud-image.org | grep freebsd | grep -E 'zfs.*\\.qcow2$' | sort -V | tail -n 1)
+
+echo y | ship --vm delete freebsd-vm-base 
+
+echo n | ship --vm create arch-vm-base --source "$LATEST_IMAGE"
+
+sed -i '/<\/devices>/i \
+  <console type="pty">\
+    <target type="virtio"/>\
+  </console>' "$XML_FILE"
+
+virsh -c qemu:///system undefine freebsd-vm-base
+virsh -c qemu:///system define "$XML_FILE"
+
+ship --vm start freebsd-vm-base 
+
+./setup.sh
+./view_vm.sh
+
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+COMMANDS=$(cat <<'EOF'
+EOF
+)
+
+while IFS= read -r command; do
+    if [[ -n "$command" ]]; then
+        tmux send-keys -t arch-vm-base "$command" C-m
+        sleep 1
+    fi
+done <<< "$COMMANDS"
+
+COMMANDS=$(cat <<EOF 
+EOF
+)
+
+tmux send-keys -t arch-vm-base "$COMMANDS" C-m
+
