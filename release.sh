a26ac6b7c0c741aa954d3878aea5ebcd8f086e5b
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-10 18:21:18 +0530

    Modified the release.sh

--- release.sh
+++ release.sh
@@ -1,23 +1,23 @@
 #!/bin/bash 
 
-echo "Shutting down the Arch VM..." 
+echo "Shutting down the FreeBSD VM..." 
 
-echo y | ship --vm shutdown arch-vm-base 
+echo y | ship --vm shutdown freebsd-vm-base 
 
-echo "Compressing the Arch VM disk image..."
+echo "Compressing the FreeBSD VM disk image..."
 
-ship --vm compress arch-vm-base 
+ship --vm compress freebsd-vm-base 
 
-echo "Copying the Arch VM disk image to generate the release package for 'arch-vm-base'..."
+echo "Copying the FreeBSD VM disk image to generate the release package for 'freebsd-vm-base'..."
 
-DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
+DISK_IMAGE=$(sudo virsh domblklist freebsd-vm-base | grep .qcow2 | awk '{print $2}')
 
-cp "$DISK_IMAGE" output/archlinux.qcow2
+cp "$DISK_IMAGE" output/freebsd.qcow2
 
 echo "Splitting the copied disk image into two parts..."
 
-split -b $(( $(stat -c%s "output/archlinux.qcow2") / 2 )) -d -a 3 "output/archlinux.qcow2" "output/archlinux.qcow2."
+split -b $(( $(stat -c%s "output/freebsd.qcow2") / 2 )) -d -a 3 "output/freebsd.qcow2" "output/freebsd.qcow2."
 
-echo "The release package for 'arch-vm-base' has been generated and split successfully!"
+echo "The release package for 'freebsd-vm-base' has been generated and split successfully!"
 
 
